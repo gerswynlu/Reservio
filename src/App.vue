f2bc73a510b29fe6293db7b22fcde11c0c35bc88
kevtiong <126160399+kevtiong@users.noreply.github.com>
2024-05-08 19:43:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+      <Header />
+      <router-view />
+    </div>
+  </template>
+  
+  <script>
+  import Header from "@/components/Header.vue";
+  
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    font-family:Arial, Helvetica, sans-serif;
+  }
+  .app {
+    display: flex;
+    height: fit-content;
+    main {
+      flex: 1 1 0;
+      padding: 2rem;
+  
+      @media (max-width: 768px) {
+        padding-left: 6rem;
+      }
+    }
+  }
+  </style>
