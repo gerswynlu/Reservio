e1553e89d8880a83752c09c0fc3e8f8abbd0fcac
kevtiong <126160399+kevtiong@users.noreply.github.com>
2024-05-09 13:21:12 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 // router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/Home.vue'; // Import your Home component
-import Restaurants from '../components/Restaurants.vue'; // Import your Restaurants component
-import Contact from '../components/Contact.vue'; // Import your Contact component
-import FAQ from '../components/FAQs.vue'; // Import your FAQ component
+import Home from '../views/Home.vue'; // Import your Home component
+import Restaurants from '../views/Restaurants.vue'; // Import your Restaurants component
+import Contact from '../views/Contact.vue'; // Import your Contact component
+import FAQs from '../views/FAQs.vue'; // Import your FAQ component
+import Wolfgang from '../views/Wolfgang.vue'; 
+import China from '@/views/China.vue';
+import Gallery from '@/views/Gallery.vue';
+import Senju from '@/views/Senju.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/restaurants', component: Restaurants },
   { path: '/contact', component: Contact },
-  { path: '/faq', component: FAQ }
+  { path: '/faq', component: FAQs },
+  { path: '/wolfgang', component: Wolfgang },
+  { path: '/china', component: China },
+  { path: '/gallery', component: Gallery },
+  { path: '/senju', component: Senju },
+  
 ];
 
 const router = createRouter({
